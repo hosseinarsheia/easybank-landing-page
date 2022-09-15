607b25abd6fa6bc395c9825fad30d0bf3897f66a
Unknown <hosseinarsheia@yahoo.com>
2022-09-15 12:28:58 +0430

    stable
    - done nav, hero, feature and articles sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        my_darkBlue: 'hsl(233, 26%, 24%)',
-        my_limegreen: 'hsl(136, 65%, 51%)',
-        my_brightCyan: ' hsl(192, 70%, 51%)',
-        my_grayishBlue: 'hsl(233, 8%, 62%)',
-        my_lightGrayishBlue: 'hsl(220, 16%, 96%)',
-        my_veryLightGray: 'hsl(0, 0%, 98%)',
-        my_white: 'hsl(0, 0%, 100%)',
+        'my-darkBlue': 'hsl(233, 26%, 24%)',
+        'my-limegreen': 'hsl(136, 65%, 51%)',
+        'my-brightCyan': ' hsl(192, 70%, 51%)',
+        'my-grayishBlue': 'hsl(233, 8%, 62%)',
+        'my-lightGrayishBlue': 'hsl(220, 16%, 96%)',
+        'my-veryLightGray': 'hsl(0, 0%, 98%)',
+        'my-white': 'hsl(0, 0%, 100%)',
       },
 
       fontSize: {
-        my_fontSize: '18px',
+        'my-fontSize': '18px',
       },
 
       fontWeight: {
-        my_fontWeight_300: '300',
-        my_fontWeight_400: '400',
-        my_fontWeight_700: '700',
+        'my-fontWeight_300': '300',
+        'my-fontWeight_400': '400',
+        'my-fontWeight_700': '700',
       },
 
       fontFamily: {
-        my_fontFamily: ['Public Sans', 'serif'],
+        'my-fontFamily': ['Public Sans', 'serif'],
+      },
+      lineHeight: {
+        'leading-12': '3.5rem',
       },
     },
   },
