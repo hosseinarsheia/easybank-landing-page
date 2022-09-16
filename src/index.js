35ed0a3a9745ba9d422c9dce3a75ff0358655f1b
hossein <hosseinarsheia@yahoo.com>
2022-09-16 13:36:22 +0430

    stable
    - Done Hamburger menu

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let hambergerMneu = document.getElementById('hambergerMenu');
+let XMark = document.getElementById('XMark');
+let floatMenu = document.getElementById('floatMenu');
+
+function hambergerMenuHandler(params) {
+  hambergerMneu.classList.toggle('hidden');
+  XMark.classList.toggle('hidden');
+  floatMenu.classList.toggle('invisible');
+}
